0e433bba1a7f3229ec6b4ff3f45da918a110adc5
Jahdona <jahdonah@yahoo.com>
2024-09-25 11:15:07 -0700

    feat: designing Header and Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>Designed and Developed by Jean de Dieu Habiyaremye</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <><header className="header">
+    <div className="photo"></div>
+    <div className="Wel"> JEAN DE DIEU HABIYAREMYE PORTIFOLIO</div>
+
+</header>
+<nav>
+    <a href="#About-Me">About Me</a>
+    <a href="#Work">Work</a>
+    <a href="#Contact-Me">Contact Me</a>
+    <a href="./assets/Document/Resume.pdf"> Resume</a>
+</nav>
+</>
+  )
+}
+
+export default Header
